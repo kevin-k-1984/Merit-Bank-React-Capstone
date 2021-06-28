a7fb48428b381cca442c13cb4cf4c144e7e24101
kevin-k-1984 <78624076+kevin-k-1984@users.noreply.github.com>
2021-06-28 15:45:59 -0700

    setup on new computer

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import { Route, withRouter, Switch } from 'react-router-dom';
-// import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
 import Header from './Header/Header';
 import Home from "./Home/Home"
 import UserDashboard from './UserDashboard/UserDashboard';
 import AdminDashboard from './AdminDashboard/AdminDashboard';
-// import { login } from '../Actions/Authentication';
 import UserDetails from './UserDetails/UserDetails';
 
 class Main extends Component {
@@ -25,15 +23,4 @@ class Main extends Component {
     }
 }
 
-// const mapStateToProps = state => ({
-//     jwt: state.jwt,
-//     role: state.role,
-//     errMess: state.errMess
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//     Login: (username, password) => dispatch(login(username, password))
-// });
-
-// export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
 export default Main;
