73003c3f5632dedcf281391fb2f3d89290581cb2
kevin-k-1984 <78624076+kevin-k-1984@users.noreply.github.com>
2021-06-28 18:00:08 -0700

    change C to lower case on component folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Main from './Components/Main';
+import Main from './components/Main';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './Reducers/ConfigureStore'
